b0f76ce0463f7abe496fd6607c6f5cead4044961
ArunKumar <arunkumar.e@outlook.com>
2022-09-12 09:25:31 +0530

    Add project files.

---
+++ JWTAuthenticationProject/Authentication/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationProject.Authentication
+{
+    public class Register
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailId { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ JWTAuthenticationProject/Controllers/AuthenticateController.cs
@@ -0,0 +1,148 @@
+﻿using JWTAuthenticationProject.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(Register register)
+        {
+            var userExists = await _userManager.FindByNameAsync(register.Username);
+            if (userExists != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+            }
+
+            ApplicationUser user = new ApplicationUser
+            {
+                Email = register.EmailId,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = register.Username
+            };
+
+            var result = await _userManager.CreateAsync(user, register.Password);
+
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed, Please check the user details and try again" });
+            }
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully" });
+        }
+
+        [HttpPost("RegisterAdmin")]
+        public async Task<IActionResult> RegisterAdmin(Register register)
+        {
+            var userExists = await _userManager.FindByNameAsync(register.Username);
+            if (userExists != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+            }
+
+            ApplicationUser user = new ApplicationUser
+            {
+                Email = register.EmailId,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = register.Username
+            };
+
+            //Create User
+            var result = await _userManager.CreateAsync(user, register.Password);
+
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed, Please check the user details and try again" });
+            }
+
+            //Checking Whether Admin is exists or not
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            {
+                //If not exists, Create Admin role
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+            }
+            //Check Whether User Role is exists or not
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+            {
+                //If not exists, Create User Role
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+            }
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            {
+                //If Admin Role exists, add role to the current user as Admin
+                await _userManager.AddToRoleAsync(user, UserRoles.Admin);
+            }
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully" });
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(Login login)
+        {
+            var user = await _userManager.FindByNameAsync(login.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, login.Password))
+            {
+                //Get the Logged in User Role
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    //Who can claim
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    //Creating new JWT Claim Name
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                //Adding the user role to the claim
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+                //Getting the secret key created in appsettings
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                //Creating JWT Security Token
+                var token = new JwtSecurityToken(
+                    //issuer: _configuration["JWT:ValidIssuer"],
+                    //audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(5),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
+
